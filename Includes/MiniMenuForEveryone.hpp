b5b841de8e7a2b5838f5dc8ea65fb49a801cee7c
fps18 <77520486+fps18@users.noreply.github.com>
2023-11-24 17:27:49 +0900

    Add files via upload

--- Includes/MiniMenuForEveryone.hpp
+++ Includes/MiniMenuForEveryone.hpp
@@ -5,7 +5,7 @@
 
 namespace CTRPluginFramework
 {
-    void Implementation_MiniMenu    (const std::string &str, u32 major, u32 minor, u32 revision, u32 Button);
+    void Implementation_MiniMenu    (const std::string &str, u32 major, u32 minor, u32 revision, u32 Button, bool Optimize = false);
 
     void new_entry (const std::string &name, short type, u32 Address, u32 Value, const std::string &note = "");
 }
--- Sources/MiniMenuForEveryone.cpp
+++ Sources/MiniMenuForEveryone.cpp
@@ -54,6 +54,7 @@ namespace CTRPluginFramework
         std::vector<Cheat_Item> Entry;                  //概要
         static bool Cheating            = false;        //一つでもチートがオンか
         static bool InformationOpened   = false;        //(i)を開いてるか
+        static bool Optimize            = false;        //軽量版かどうか
         std::string             Copyright = "Basic developer: FPS 18";
     }
 
@@ -61,20 +62,19 @@ namespace CTRPluginFramework
     void new_entry (const std::string &name, short type, u32 Address, u32 Value, const std::string &note)
     {
         //重複確認
-        bool isDuplicate = false;
+        bool Duplicate = false;
         for (const Cheat_Item &entry : MiniMenuOverview::Entry)
         {
             if (entry.name == name && entry.type == type && entry.Address == Address && entry.Value == Value && entry.note == note)
             {
-                isDuplicate = true;
+                Duplicate = true;
                 break;
             }
         }
-        //名前の長さチェック
-
-        if (!isDuplicate)
+        if (!Duplicate)
         {
             Cheat_Item newItem;
+            //////////////////
             newItem.name    =   name;
             newItem.note    =   note;
             newItem.type    =   type;
@@ -94,10 +94,11 @@ namespace CTRPluginFramework
     //const std::string str = title
     //const char ver = version
     //u32 Button = Buttons to display on the display
-    void Implementation_MiniMenu (const std::string &str, u32 major, u32 minor, u32 revision, u32 Button)
+    void Implementation_MiniMenu (const std::string &str, u32 major, u32 minor, u32 revision, u32 Button, bool Optimize)
     {    
-        MiniMenuOverview::TitleName = str;  //タイトルの名前代入
-        static bool MenuOpened = false;         //メニューが開いてるか判別するやつ
+        MiniMenuOverview::Optimize  = Optimize; //軽量化かどうか
+        MiniMenuOverview::TitleName = str;      //タイトルの名前代入
+        static bool MenuOpened      = false;    //メニューが開いてるか判別するやつ
                                                 //staticにすると何回読み込んでも上書きされない
         //ver代入
         MiniMenuOverview::VerString = "[" + Utils::Format("%d.%d.%d", major, minor, revision) + "]";
@@ -167,8 +168,17 @@ namespace CTRPluginFramework
         }
 
         //タイトル下線部
-        const int TitleTextWidth = OSD::GetTextWidth(true, MiniMenuOverview::TitleName);
-        screen.DrawRect(37, 42, 32 + TitleTextWidth, 1, Color::White);
+        if (!MiniMenuOverview::Optimize)
+        {
+            //非軽量時
+            const int TitleTextWidth = OSD::GetTextWidth(true, MiniMenuOverview::TitleName);
+            screen.DrawRect(37, 42, 32 + TitleTextWidth, 1, Color::White);
+        }
+        else
+        {
+            //軽量時
+            screen.DrawRect(37, 42, 100, 1, Color::White);
+        }
 
         //コピーライトの描写
         const int Copyright_posX = ((320 - OSD::GetTextWidth(false, MiniMenuOverview::Copyright)) / 2);
@@ -205,26 +215,46 @@ namespace CTRPluginFramework
                     screen.DrawRect(xPos_CheckBox, yPos_CheckBox + (15 * i), 9, 9, Color::White);//■
                     if (MiniMenuOverview::Entry[i + scroll].ProgramRunning) 
                     {
-                        //有効時の描写
-                        screen.DrawPixel(xPos_CheckBox + 7, yPos_CheckBox +  1 + (15 * i), Color::LimeGreen);
-                        for (int j = 0; j < 2; j++)
+                        if (!MiniMenuOverview::Optimize)
                         {
-                            screen.DrawPixel(xPos_CheckBox + 7 - j, yPos_CheckBox +  2 + (15 * i), Color::LimeGreen);
-                            screen.DrawPixel(xPos_CheckBox + 6 - j, yPos_CheckBox +  3 + (15 * i), Color::LimeGreen);
-                            screen.DrawPixel(xPos_CheckBox + 5 - j, yPos_CheckBox +  4 + (15 * i), Color::LimeGreen);
-                            screen.DrawPixel(xPos_CheckBox + 4 - j, yPos_CheckBox +  5 + (15 * i), Color::LimeGreen);
-                            screen.DrawPixel(xPos_CheckBox + 3 - j, yPos_CheckBox +  6 + (15 * i), Color::LimeGreen);
-                            screen.DrawPixel(xPos_CheckBox + 2 - j, yPos_CheckBox +  5 + (15 * i), Color::LimeGreen);
+                            //有効時の描写(非軽量化)
+                            screen.DrawPixel(xPos_CheckBox + 7, yPos_CheckBox +  1 + (15 * i), Color::LimeGreen);//Top
+                            screen.DrawPixel(xPos_CheckBox + 7, yPos_CheckBox +  2 + (15 * i), Color::LimeGreen);// s
+                            screen.DrawPixel(xPos_CheckBox + 6, yPos_CheckBox +  3 + (15 * i), Color::LimeGreen);// s
+                            screen.DrawPixel(xPos_CheckBox + 5, yPos_CheckBox +  4 + (15 * i), Color::LimeGreen);// s
+                            screen.DrawPixel(xPos_CheckBox + 4, yPos_CheckBox +  5 + (15 * i), Color::LimeGreen);// s
+                            screen.DrawPixel(xPos_CheckBox + 3, yPos_CheckBox +  6 + (15 * i), Color::LimeGreen);// s
+                            screen.DrawPixel(xPos_CheckBox + 2, yPos_CheckBox +  5 + (15 * i), Color::LimeGreen);// s
+                            screen.DrawPixel(xPos_CheckBox + 6, yPos_CheckBox +  2 + (15 * i), Color::LimeGreen);//ss
+                            screen.DrawPixel(xPos_CheckBox + 5, yPos_CheckBox +  3 + (15 * i), Color::LimeGreen);//ss
+                            screen.DrawPixel(xPos_CheckBox + 4, yPos_CheckBox +  4 + (15 * i), Color::LimeGreen);//ss
+                            screen.DrawPixel(xPos_CheckBox + 3, yPos_CheckBox +  5 + (15 * i), Color::LimeGreen);//ss
+                            screen.DrawPixel(xPos_CheckBox + 2, yPos_CheckBox +  6 + (15 * i), Color::LimeGreen);//ss
+                            screen.DrawPixel(xPos_CheckBox + 1, yPos_CheckBox +  5 + (15 * i), Color::LimeGreen);//ss
+                        }
+                        else
+                        {
+                            //有効時の描写(軽量版)
+                            screen.DrawRect(xPos_CheckBox + 1, yPos_CheckBox + 1 + (15 * i), 7, 7, Color::LimeGreen);
                         }
                     }
                 }
                 else if (MiniMenuOverview::Entry[i + scroll].type == 1) //チェック不可
                 {
-                    screen.DrawRect(xPos_CheckBox + 2, yPos_CheckBox     + (15 * i), 5, 2, Color::White);
-                    screen.DrawRect(xPos_CheckBox    , yPos_CheckBox + 2 + (15 * i), 2, 5, Color::White);
-                    screen.DrawRect(xPos_CheckBox + 2, yPos_CheckBox + 7 + (15 * i), 5, 2, Color::White);
-                    screen.DrawRect(xPos_CheckBox + 7, yPos_CheckBox + 2 + (15 * i), 2, 5, Color::White);
-                    screen.DrawRect(xPos_CheckBox + 3, yPos_CheckBox + 3 + (15 * i), 3, 3, Color::White, false);
+                    if (!MiniMenuOverview::Optimize)
+                    {
+                        //非軽量
+                        screen.DrawRect(xPos_CheckBox + 2, yPos_CheckBox     + (15 * i), 5, 2, Color::White);
+                        screen.DrawRect(xPos_CheckBox    , yPos_CheckBox + 2 + (15 * i), 2, 5, Color::White);
+                        screen.DrawRect(xPos_CheckBox + 2, yPos_CheckBox + 7 + (15 * i), 5, 2, Color::White);
+                        screen.DrawRect(xPos_CheckBox + 7, yPos_CheckBox + 2 + (15 * i), 2, 5, Color::White);
+                        screen.DrawRect(xPos_CheckBox + 3, yPos_CheckBox + 3 + (15 * i), 3, 3, Color::White, false);
+                    }
+                    else
+                    {
+                        //軽量
+                        screen.DrawRect(xPos_CheckBox, yPos_CheckBox + (15 * i), 9, 9, Color::Yellow);
+                    }
                 }
 
                 //(i)の描写
